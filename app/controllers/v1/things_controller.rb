7875d170e7a104cc44ae7e20bd7536fc981cb656
blakbox23 <boudii30@gmail.com>
2021-11-08 23:34:11 +0300

    initial app and db setup

---
+++ app/controllers/v1/things_controller.rb
@@ -0,0 +1,10 @@
+class V1::ThingsController < ApplicationController
+  def index 
+    render json: { :things => [
+       { 
+        :name => 'some-thing',
+        :guid => '1'
+    }
+    ] }.to_json
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,5 @@
+Rails.application.routes.draw do
+  namespace :v1, defaults: { format: 'json'} do
+    get 'things', to: 'things#index'
+  end 
+end
