489512d845a392dc359bbea6e2ecc0ab32b2eec5
blakbox23 <boudii30@gmail.com>
2021-11-09 09:52:25 +0300

    Add react components route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   root 'static#index'
+  
+  get '*page', to: 'static#index', constraints: ->(req) do
+    !req.xhr? && req.format.html?
+  end
+  
   namespace :v1, defaults: { format: 'json'} do
     get 'things', to: 'things#index'
     resources :greetings, only: [:index]
